268121421747f934f93033543e92e2e496d8cdb7
shashankdofficial <allyouget14@gmail.com>
2023-01-20 11:15:34 +0530

    Update

--- test/src/com/masai/Main.java
+++ test/src/com/masai/Main.java
@@ -1,47 +1,24 @@
 package com.masai;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main{
 	
-//	@Override
-//	public void run() {
-//		System.out.println("Thread is running");
-//	}
-	
 	public static void main(String[] args) {
-	
-//		 Main main = new Main();
-//		 main.start();
-//		 Test t1 = new Test(10,20);
 		
-//		TreeMap<Integer,Integer> map = new TreeMap<>();
-//		
-//		map.put(5, 5);
-//		map.put(4, 4);
-//		map.put(1, 1);
-//		map.put(3, 3);
-//		map.put(2, 2);
-//		
-//		map.remove(1);
-//		
-//		List<Integer> list = new ArrayList<>();
-//		map.entrySet().forEach(a ->{list.add(a.getKey())});
-//		
-//		
-//		for(Map.Entry<Integer, Integer> i: map.entrySet()) {
-//			i.getValue();
-//		}
+		int[] arr = {3,2,3,4,3,5,2};
 		
+//		Set<Integer> set = new HashSet<>();
+		Map<Integer,Integer> map = new HashMap<>();
 		
+		for(int i=0; i<arr.length; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+		}
 		
-		StringBuffer str = new StringBuffer("Manoj");
-		
-		str.append(" Tiwari");
-		
-		System.out.println(str);
+		for(Map.Entry<Integer, Integer> i : map.entrySet()) {
+			if(i.getValue() > 1) {
+				System.out.println(i.getKey());
+			}
+		}
 	}
 }
